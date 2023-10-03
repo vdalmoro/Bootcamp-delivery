a16b21630deb9b489261bb98d9a6bd193ff97055
Vinicius Godinho Dal Moro <vinimegasxlr@gmail.com>
2023-10-03 11:53:08 -0300

    updatecode2
    Create function  criarPedido

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+import { timeStamp } from "console";
 import express from "express";
-import { promises as fs, read, readFileSync, write } from "fs";
+import { promises as fs, read, readFileSync, write, writeFileSync } from "fs";
 
 const { readFile, writeFile } = fs;
 const app = express();
@@ -11,8 +12,8 @@ app.listen(3000, async () => {
     console.log("API Started!");
   } catch (err) {
     const initialJson = {
-      brand: "Teste",
-      models: [],
+      nextId: 1,
+      pedidos: [],
     };
     writeFile("pedidos.json", JSON.stringify(initialJson))
       .then(() => {
@@ -28,3 +29,33 @@ app.listen(3000, async () => {
 app.get("/teste", (req, res) => {
   res.send(req.method);
 });
+
+//Chamada criarPedido
+app.post("/criarPedido", (req, res) => {
+  const { cliente, produto, valor } = req.body;
+
+  if (!cliente || !produto || !valor) {
+    return res.json({
+      error: "Cliente, produto e valor são campos obrigatórios.",
+    });
+  }
+  return res.json(criarPedido(cliente, produto, valor));
+});
+
+//Função criarPedido
+function criarPedido(cliente, produto, valor) {
+  const pedidosData = JSON.parse(readFileSync("pedidos.json", "utf8"));
+  const novoPedido = {
+    id: pedidosData.nextId,
+    cliente,
+    produto,
+    valor,
+    entregue: false,
+    timestamp: new Date(),
+  };
+
+  pedidosData.nextId++;
+  pedidosData.pedidos.push(novoPedido);
+  writeFileSync("./pedidos.json", JSON.stringify(pedidosData, null, 2));
+  return novoPedido;
+}
